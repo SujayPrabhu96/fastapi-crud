197677d8b054c7661016a0e6e90563241eb48565
SujayPrabhu96 <prabhusujay@gmail.com>
2025-12-02 13:09:51 +0530

    refactor database session management by moving get_db function to db.py

--- app/api/v1/todo.py
+++ app/api/v1/todo.py
@@ -3,14 +3,7 @@ from sqlalchemy.orm import Session
 
 from app.models.todo import TodoRead, TodoCreate
 from app.services.todo_service import TodoService
-from app.db.db import SessionLocal
-
-def get_db():
-  db = SessionLocal()
-  try:
-    yield db
-  finally:
-    db.close()
+from app.db.db import get_db
 
 router = APIRouter(
   prefix="/api/v1",
